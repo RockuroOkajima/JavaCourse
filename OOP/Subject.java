4e69a6a2befe6133bb5fb3e8330b36ac6be204ee
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-01-05 22:08:12 +0200

    Create Subject.java

---
+++ OOP/Subject.java
@@ -0,0 +1,21 @@
+public class Subject {
+    String name;
+    int mark;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+}
