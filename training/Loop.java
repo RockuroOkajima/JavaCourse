62a39fe0a31f37c0a9a0257508e0588b0904fd42
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-01-15 23:38:14 +0200

    Create Loop.java

---
+++ training/Loop.java
@@ -0,0 +1,8 @@
+class Example {
+    public static void main(String args[]) {
+        int x;
+  
+        for(x = 0; x<10; x++) // x++ equal to x = x + 1
+          System.out.println("This is x: " + x);
+    }
+  }
