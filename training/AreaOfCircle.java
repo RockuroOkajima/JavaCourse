ffef6ffea9a549d45bec7968b45028053d8818bd
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-01-16 00:52:18 +0200

    Create AreaOfCircle.java

---
+++ training/AreaOfCircle.java
@@ -0,0 +1,9 @@
+class Example {
+    
+            public static void main(String args[]) {
+              double pi  = 3.1416, r = 10.8, a = pi * r * r;
+          
+              System.out.println("Area of circle is " + a);
+            }
+
+        } 
